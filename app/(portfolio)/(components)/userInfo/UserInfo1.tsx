e3c9d0cb90a47bfcdd8e71545c5062db497bdd0d
Greg057 <88341092+Greg057@users.noreply.github.com>
2025-01-24 18:18:58 +0100

    fix mobile responsiveness

--- app/(portfolio)/(components)/userInfo/UserInfo1.tsx
+++ app/(portfolio)/(components)/userInfo/UserInfo1.tsx
@@ -4,7 +4,6 @@ import { Mail, Github, Linkedin, Twitter, Download, MapPin } from 'lucide-react'
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Button } from "@/components/ui/button"
 import { UserInfo } from '@/types/supabase-types'
-import { Card, CardContent } from '@/components/ui/card'
 import { useEffect, useState } from 'react'
 import { createClient } from '@/utils/supabase/client'
 
@@ -45,64 +44,71 @@ export default function UserInfo1({ personalInfo }: { personalInfo: UserInfo }) 
   }, []);
 
   return (
-    <header className="py-20 flex flex-col items-center justify-center">
-        <Avatar className="w-32 h-32 mx-auto mb-8 border-4 border-primary" >
-          <AvatarImage src={avatarUrl} alt={`${personalInfo.full_name}'s avatar`} />
-          <AvatarFallback>{personalInfo.full_name.charAt(0)}</AvatarFallback>
-        </Avatar>
-        <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold mb-4 bg-clip-text">
-          {personalInfo.full_name}
-        </h1>
-        {personalInfo.title && (
-          <p className="text-xl sm:text-2xl mb-2 text-muted-foreground">{personalInfo.title}</p>
-        )}
-        {personalInfo.location && (
-          <p className="text-lg mb-8 text-muted-foreground flex items-center">
-            <MapPin className="h-5 w-5 mr-2" />
-            {personalInfo.location}
-          </p>
-        )}
-        <div className="flex justify-center space-x-4 mb-8">
+    <header className="py-20 px-3 flex flex-col items-center justify-center">
+      <Avatar className="w-32 h-32 mx-auto mb-8 border-4 border-primary">
+        <AvatarImage src={avatarUrl} alt={`${personalInfo.full_name}'s avatar`} />
+        <AvatarFallback>{personalInfo.full_name.charAt(0)}</AvatarFallback>
+      </Avatar>
+      
+      <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold mb-4 bg-clip-text">
+        {personalInfo.full_name}
+      </h1>
+      
+      {personalInfo.title && (
+        <p className="text-xl sm:text-2xl mb-2 text-muted-foreground">
+          {personalInfo.title}
+        </p>
+      )}
+  
+      {personalInfo.location && (
+        <p className="text-lg mb-8 text-muted-foreground flex items-center">
+          <MapPin className="h-5 w-5 mr-2" />
+          {personalInfo.location}
+        </p>
+      )}
+  
+      <div className="flex flex-wrap justify-center gap-4 mb-8">
+        <Button variant="outline" size="icon" asChild>
+          <a href={`mailto:${personalInfo.email}`} aria-label="Email">
+            <Mail className="h-5 w-5" />
+          </a>
+        </Button>
+        {personalInfo.github && (
           <Button variant="outline" size="icon" asChild>
-            <a href={`mailto:${personalInfo.email}`} aria-label="Email">
-              <Mail className="h-5 w-5" />
+            <a href={personalInfo.github} target="_blank" rel="noopener noreferrer" aria-label="GitHub">
+              <Github className="h-5 w-5" />
             </a>
           </Button>
-          {personalInfo.github && (
-            <Button variant="outline" size="icon" asChild>
-              <a href={personalInfo.github} target="_blank" rel="noopener noreferrer" aria-label="GitHub">
-                <Github className="h-5 w-5" />
-              </a>
-            </Button>
-          )}
-          {personalInfo.linkedin && (
-            <Button variant="outline" size="icon" asChild>
-              <a href={personalInfo.linkedin} target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
-                <Linkedin className="h-5 w-5" />
-              </a>
-            </Button>
-          )}
-          {personalInfo.x && (
-            <Button variant="outline" size="icon" asChild>
-              <a href={personalInfo.x} target="_blank" rel="noopener noreferrer" aria-label="Twitter">
-                <Twitter className="h-5 w-5" />
-              </a>
-            </Button>
-          )}
-          {cvUrl && (
-          <Button className="mb-12" asChild>
-            <a href={cvUrl} download aria-label="Download CV">
+        )}
+        {personalInfo.linkedin && (
+          <Button variant="outline" size="icon" asChild>
+            <a href={personalInfo.linkedin} target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
+              <Linkedin className="h-5 w-5" />
+            </a>
+          </Button>
+        )}
+        {personalInfo.x && (
+          <Button variant="outline" size="icon" asChild>
+            <a href={personalInfo.x} target="_blank" rel="noopener noreferrer" aria-label="Twitter">
+              <Twitter className="h-5 w-5" />
+            </a>
+          </Button>
+        )}
+        {cvUrl && (
+          <Button asChild>
+            <a href={cvUrl} target="_blank" download aria-label="Download CV">
               <Download className="h-5 w-5 mr-2" />
               Download CV
             </a>
           </Button>
         )}
-        </div>
-        {personalInfo.about_me && ( 
-          <p className="mx-auto text-center px-[15%]">
-            {personalInfo.about_me}
-          </p>
-        )}
+      </div>
+  
+      {personalInfo.about_me && (
+        <p className="mx-auto text-center sm:px-[25%]">
+          {personalInfo.about_me}
+        </p>
+      )}
     </header>
-  )
+  );  
 }
